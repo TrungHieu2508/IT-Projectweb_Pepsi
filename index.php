b8c56d638e1925f5de0e0c6429faf369dc0ba514
Nguyenthlinh <162722526+Nguyenthlinh@users.noreply.github.com>
2025-03-05 15:14:20 +0700

    update

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Quản lí khách hàng</title>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
-    include "Model/DBconfig.php";
-    $db = new Database;
-    $db->connect();
+require_once "./mvc/Bridge.php";
+$myApp = new App();
 
-    if(isset($_GET['controller'])){
-        $controller = $_GET['controller'];
-    }else{
-        $controller = '';
-    }
-    switch ($controller) {
-        case 'user':{
-            require_once('Controller/user/index.php');
-        }
-            
-    
-}
 ?>
