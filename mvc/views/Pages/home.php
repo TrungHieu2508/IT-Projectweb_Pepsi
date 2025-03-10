fbf55d52418783326c0eff00147b8ef9f03f022f
Nguyenthlinh <162722526+Nguyenthlinh@users.noreply.github.com>
2025-03-10 16:29:37 +0700

    update

--- mvc/views/Pages/home.php
+++ mvc/views/Pages/home.php
@@ -1,25 +1,67 @@
 <?php 
     require_once "./mvc/views/blocks/header.php"; 
 ?>
+<div class="app__container">
 
-        <!-- Container -->
-        <div class="app__container">
-           <div class="app__container__welcome">
-                <div class="welcome__img"><img src="./public/img/pepsi-0sugar.png" alt="pepsi nosugar"></div>
-                <div class="welcome__heading">
-                    <div class="welcome__heading__content">
-                        <h1>WELLCOME</h1>
-                        <h2>LET'S GET STARTED</h2>
-                        <h3>CHECK OUT THE LATEST DROP</h3>
-                    </div>
-                    <div class="welcome__heading--down"><img src="./public/img/downward-arrow.png" alt="mui ten huong xuong"></div>
-                </div>
-           </div>
-           
-        
-           
+<div class="app__container__welcome">
+     <div class="welcome__img"><img src="./public/img/pepsi-0sugar.png" alt="pepsi nosugar"></div>
+     <div class="welcome__heading">
+         <div class="welcome__heading__content">
+             <h1>WELLCOME</h1>
+             <h2>LET'S GET STARTED</h2>
+             <h3>CHECK OUT THE LATEST DROP</h3>
+         </div>
+         <div class="welcome__heading--down"><a href="#down"><img src="./public/img/downward-arrow.png" alt="mui ten huong xuong"></a></div>
+     </div>
+</div>
 
-        </div>
+<div class="app__container__style" id="down">
+     <div class="style__img__jacket"> <img src="./public/img/denim cherry front.jpg" alt="denim jacket with pepsi"></div>
+     <div class="style__content" style="display: flex; flex-direction: column;">
+         <div class="content__img"><img src="./public/img/content pepsi.png" alt="content denim jacket"></div>
+         <div class="content__text" style="font-size: 23px; line-height: 20px; font-weight: 900; margin-left: 80px;margin-top: 15px;"><p>Enter now for a chance to win this limited-</p><br><p>edition item. *</p></div>
+         <a href="/Git/IT-Projectweb_Pepsi/Login"><button class="content__sign--button">SIGN IN TO ENTER</button></a>
+         <div class="content__msg"><p>U.S. res, 18+ (19+ in AL/NE). Ends 3/31/25. Limit one entry per person/email address </p><br><p>per day. Subject to Official Rules.</p></div>
+     </div>
+</div>
+
+<div class="app__container__viewProduct">
+     <div class="viewProduct__video">
+         <video src="./public/img/Pepsi Product Carousel Video.mp4" controls autoplay loop muted></video>
+     </div>
+     <a href="/Git/IT-Projectweb_Pepsi/Product"><button class="viewProduct__button">VIEW PRODUCTS</button></a>
+</div>
+
+<div class="app__container__checkItOut">
+     <!-- video 1 -->
+     <div class="checkItOut__video" style="position: relative;bottom: 65px;">
+         <iframe width="100%" height="850px"src="https://www.youtube.com/embed/5TwKmdGUExY?autoplay=1&mute=1&loop=1&playlist=5TwKmdGUExY&si=Ke8tWC1GhpE7TFtU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </div>
+     <a href="https://www.youtube.com/watch?v=5TwKmdGUExY&t=3s"><button class="checkItOut__button">CHECK IT OUT</button></a>
+     <!-- video 2 -->
+     <div class="checkItOut__video" style="position: relative;bottom: 130px">
+         <iframe width="100%" height="850px" src="https://www.youtube.com/embed/kWzqy9qsmKI?autoplay=1&mute=1&loop=1&playlist=kWzqy9qsmKI&si=T5wHEsp6O8UxXyNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </div>
+     <a href="https://www.youtube.com/watch?v=kWzqy9qsmKI"><button class="checkItOut__button" style="bottom: 270px;">CHECK IT OUT</button></a>
+</div>
+
+<div class="app__container__viewRecipes">
+     <div class="viewRecipes__video" style="position: relative; bottom:195px;">
+         <video src="./public/img/Marketing pepsi video.mp4" controls autoplay loop muted></video>
+     </div>
+     <a href="#"><button class="viewRecipes__button">VIEW RECIPES</button></a>
+</div>
+
+<div class="app__container__pulse">
+     <div class="pulse__logo"><img src="./public/img/logoweb.png" alt="logo"></div>
+     <div class="pulse__content">
+         <h1>STAY ON THE</h1><br><h1 style="font-size: 130px; color:#0025ff; margin-top: 40px;">PULSE</h1>
+     </div>
+</div>
+
+
+</div>
+
 
 <?php 
     require_once "./mvc/views/blocks/footer.php"; 
